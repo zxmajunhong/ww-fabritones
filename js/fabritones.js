07798748c46b2d82565e6559302cdcbbf3398e35
majunhong <569847911@qq.com>
2017-06-10 16:39:35 +0800

    1

--- js/fabritones.js
+++ js/fabritones.js
@@ -14,7 +14,53 @@ $(function(){
 		$(".select-list").slideUp()
 	});
 
-	$(".btn-box .buy-btn").hover(function(){
+	$(".btn-box .buy-btn").click(function(event){
+		var e = window.event || event;
+		if(e.stopPropagation){
+			e.stopPropagation();
+		}else{
+			e.cancelBubble = true;
+		}
+		showBuyFc($(this).data("amazon"),$(this).data("wish"));
+	});
+
+	$("body").on("click",".buy-fc",function(event){
+		var e = window.event || event;
+		if(e.stopPropagation){
+			e.stopPropagation();
+		}else{
+			e.cancelBubble = true;
+		}
+		$(".buy-fc").hide();
+	})
+
+	document.onclick=function(){
+		$(".buy-fc").hide();
+	}
+
+	function showBuyFc(amazon,wish){
+		if($(".buy-fc").length > 0){
+			$(".buy-fc #amazon-href").attr("href",amazon);
+			$(".buy-fc #wish-href").attr("href",wish);
+			$(".buy-fc").show();
+		}else{
+			var html = '<div class="buy-fc">'+
+						'<p class="buy-tips">You can choose these websites to purchase</p>'+
+						'<div class="buy-logo-box">'+
+							'<a id="amazon-href" href="'+amazon+'" target="_blank" class="buy-btn">'+
+								'<img src="img/fcbuy_amazon.logo.png" alt="" width="235px" height="106px">'+
+							'</a>'+
+							'<span class="sep"></span>'+
+							'<a id="wish-href" href="'+wish+'" target="_blank" class="buy-btn">'+
+								'<img src="img/wish_logo.png">'+
+							'</a>'+
+						'</div>'+
+						'<div>'
+			$("body").append(html);
+		}
+	}
+
+	/*$(".btn-box .buy-btn").hover(function(){
 		$(".amazon").slideDown();
 	},function(){
 		var hovertime = setTimeout(function(){$(".amazon").slideUp();},200);
@@ -24,7 +70,7 @@ $(function(){
 		},function(){
 			setTimeout(function(){$(".amazon").slideUp();},200)
 		});
-	});
+	});*/
 
 	//商品切换
 	$(".small-btn").on("click",function(e){
